bdd2fa5e6bf38ea37e264364655b165844aa12b7
Shadowrs <tardisfan121@gmail.com>
2022-09-22 15:17:40 +0100

    cleanup

---
+++ src/KotlinLambdasDemo2.kt
@@ -0,0 +1,47 @@
+
+import ThreadSchedulerDemo.Script
+import kotlin.system.exitProcess
+
+fun main() {
+    System.out.println("[App " + Misc.timestamp() + "] begin")
+    KotlinLambdasDemo2.test() // kick off a kotlin written task
+    ThreadSchedulerDemo.mainAppLoop()
+}
+
+/**
+ * @author Jak Shadowrs (tardisfan121@gmail.com)
+ */
+object KotlinLambdasDemo2 {
+
+    // p.s test() method body hotswap will work, but standard Java does not support hotswap of Anonymous classes (lambda expressions: code within the run({}) block)
+    fun test() {
+
+        onInteraction {
+            printf("kotlin demo 2 start")
+            wait(1)
+            printf("mid 1")
+            wait(1)
+            printf("mid 2")
+            wait(1)
+            printf("mid 3")
+            wait(1)
+            printf("mid 4")
+            wait(1)
+            printf("mid 5")
+            wait(1)
+            printf("mid 6")
+            wait(1)
+            printf("mid 7")
+            wait(1)
+            printf("mid 8")
+            wait(1)
+            exitProcess(0)
+        }
+
+    }
+
+    // method with context receiver so you can be even more lazy
+    fun onInteraction(task: Script.() -> Unit) {
+        ThreadSchedulerDemo.run(task)
+    }
+}
--- src/KotlinLambdasDemo3.kt
+++ src/KotlinLambdasDemo3.kt
@@ -3,22 +3,25 @@ import ThreadSchedulerDemo.Script
 
 fun main() {
     System.out.println("[App " + Misc.timestamp() + "] begin")
-    KotlinLambdasDemo.test() // kick off a kotlin written task
+    KotlinLambdasDemo3.test() // kick off a kotlin written task
     ThreadSchedulerDemo.mainAppLoop()
 }
 
 /**
  * @author Jak Shadowrs (tardisfan121@gmail.com)
  */
-object KotlinLambdasDemo {
+object KotlinLambdasDemo3 {
 
     // p.s test() method body hotswap will work, but standard Java does not support hotswap of Anonymous classes (lambda expressions: code within the run({}) block)
     fun test() {
-
-        ThreadSchedulerDemo.run {
-            it.printf("kotlin demo 1 start")
-            it.wait(2)
-            it.printf("wow lol")
+        onInteraction {
+            printf("kotlin demo 3 start")
+            var i = 10
+            wait(1)
+            while (i-- > 0) {
+                wait(1)
+                printf("demo 3- part $i")
+            }
         }
     }
 
